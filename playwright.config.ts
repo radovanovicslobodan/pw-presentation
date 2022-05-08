ba190606b243d69352907f4db216dc76c523b273
radovanovicslobodan <r.slobodan75@gmail.com>
2022-05-09 00:37:29 +0200

    viewport porperty added

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,7 @@ const config: PlaywrightTestConfig = {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     headless: false,
+    viewport: null,
   },
 
   /* Configure projects for major browsers */
--- tests/annotations/annotations.test.ts
+++ tests/annotations/annotations.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test("first test", async ({ page }) => {
   console.log("first test running");
