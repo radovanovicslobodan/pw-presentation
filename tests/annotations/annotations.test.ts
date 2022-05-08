dfa1d6051e4f6d46c1fe97753127c4d93b858a88
radovanovicslobodan <r.slobodan75@gmail.com>
2022-05-09 00:30:33 +0200

    annotations added

---
+++ tests/annotations/annotations.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "@playwright/test";
+
+test("first test", async ({ page }) => {
+  console.log("first test running");
+  await page.goto("https://playwright.dev");
+  console.log("first test completed");
+});
+
+test("second test", async ({ page }) => {
+  console.log("second test running");
+  await page.goto("https://www.saucedemo.com/");
+  console.log("second test completed");
+});
+
+test("third test", async ({ page, browserName }) => {
+  if (browserName === "chromium") {
+    test.skip();
+  }
+  console.log("third test running");
+  await page.goto("https://demoqa.com/");
+  console.log("third test completed");
+});
