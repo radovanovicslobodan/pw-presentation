573cfb458320e3ab2ada11f23684fe025ed7beb8
radovanovicslobodan <r.slobodan75@gmail.com>
2022-05-09 23:54:52 +0200

    added auth test

---
+++ tests/network/auth.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Network - Playwright", () => {
+  test("Http Authentication", async ({ browser }) => {
+    const context = await browser.newContext({
+      httpCredentials: {
+        username: "admin",
+        password: "admin",
+      },
+    });
+    const page = await context.newPage();
+    await page.goto("https://the-internet.herokuapp.com/basic_auth");
+    const header = await page.$("h3");
+    if (header) {
+      console.log(await header.textContent());
+      expect(await header.textContent()).toBe("Basic Auth");
+    }
+    await page.waitForTimeout(5000);
+  });
+});
